68107f77639ea9874205c6f0f91f740dfc8c7b4d
Rev909 <mathias.edouin.du78@gmail.com>
2017-01-14 14:57:08 +0100

    Révision 1.0

--- src/DataAnalysis.scala
+++ src/DataAnalysis.scala
@@ -7,9 +7,10 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.log4j.{Level, Logger}
 
-import org.apache.spark.sql.types._
 import com.github.nscala_time._
+import com.github.nscala_time.time._
 
+import java.sql.Timestamp
 
 object DataAnalysis {
   def main(args: Array[String]): Unit = {
@@ -18,12 +19,9 @@ object DataAnalysis {
     val rootlogger = Logger.getRootLogger().setLevel(Level.ERROR)
     val sqlcontext = new SQLContext(scontext)
 
-    val format = new java.text.SimpleDateFormat("mm/dd/yyyy")
-    format.format(new java.util.Date())
-
     val temp = sqlcontext.read.option("header","true").csv("SalesJan2009.csv")
     val df = temp.selectExpr(
-      "cast(Transaction_date as date) Transaction_date",
+      "cast(Transaction_date as Timestamp) Transaction_date",
       "Product",
       "cast(Price as int) Price",
       "Payment_Type",
@@ -37,6 +35,7 @@ object DataAnalysis {
       "cast(Longitude as float) Longitude")
 
     df.printSchema()
+    df.select("Transaction_date").show()
 
     /* NUMBER OF PAYMENTS */
     println("---------------------------\n")
@@ -50,11 +49,13 @@ object DataAnalysis {
     /* SALES - FIRST HALF OF THE MONTH */
     println("\nSALES FOR THE FIRST HALF OF THE MONTH")
 
-    val date = ("1/1/2009").toString(Static)
 
-    df.select("Product").filter(
-      "Transaction_date" >= fmt
-    )
+    val prod = df.select("Product")
+      .filter(s"Transaction_date >= 1/1/2009")
+      .filter(s"Transaction_date <= 1/15/2009")
+      .count()
+
+    println(s"There is $prod sold")
     print("---------------------------\n")
   }
 
