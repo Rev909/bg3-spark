a5e339ab8027af511db9e0c1d23119d10a04c7e8
Bruno-T <Bruno-T@users.noreply.github.com>
2017-01-15 22:42:45 +0100

    Add Q6, Q7, Q8
    Add Q6, Q7, Q8

--- src/DataAnalysis.scala
+++ src/DataAnalysis.scala
@@ -4,11 +4,14 @@
 
 import java.sql.Timestamp
 
+import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.sql._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SQLContext
+import java.text.SimpleDateFormat
 
 import java.text.SimpleDateFormat
 
@@ -79,6 +82,17 @@ object DataAnalysis {
     }
     println(s"On average there is ${AvgDays.toDouble.toInt} days passing between account creation and first purchase")
     println("---------------------------\n")
+   
+    //Q6
+    df.select("Product", "Price").groupBy("Product").sum("Price").show()
+
+    //Q7
+    val df3 = df.select(df("Product"), df("Price") + 2000)
+    df3.show()
+    df3.write.format("com.databricks.spark.csv").save("myFile.csv")
+
+    //Q8
+    df.select("Latitude", "Longitude").groupBy("Latitude", "Longitude").count().orderBy("count").filter("count > 2").show()
   }
 
 }
